4affb31a38eb9bddec59080e2f7ad02e0c032dcf
Alexander Lang <alex@upstre.am>
2014-07-20 11:02:48 +0200

    adds table with all videos

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,7 +1,19 @@
+require 'csv'
+
 class VideosController < ApplicationController
   helper_method :tags
   def index
     @videos = Video.todo.page(params[:page])
+  end
+
+  def all
+    @videos = Video.all.includes(:tags)
+    respond_to do |f|
+      f.html
+      f.csv do
+        render text: videos_csv(@videos), content_type: 'text/csv'
+      end
+    end
   end
 
   def search
@@ -22,8 +34,19 @@ class VideosController < ApplicationController
   end
 
   protected
-    def tags
-      @tags ||= Video.tag_counts_on(:tags)
+
+  def videos_csv(videos)
+    CSV.generate do |csv|
+      csv << %w(Id Filename Description Category Rating Tags)
+      videos.each do |v|
+        csv << [v.id, v.filename, v.description, v.category,
+          v.rating, v.tags.map(&:name).join(', ')]
+      end
     end
+  end
+
+  def tags
+    @tags ||= Video.tag_counts_on(:tags)
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root 'videos#index'
-  resources :videos
+  resources :videos do
+    collection do
+      get :all
+    end
+  end
   get '/tags/:tag' => 'videos#search', as: 'tag'
 end
